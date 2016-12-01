f0cab9f8cd4fbb6a2b85240cbd6982fcd778418e
Jafte <jafte@ya.ru>
2016-12-02 01:45:44 +0300

    more style

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import TemplateView
-from sound.models import Sound, Author, Request
+from sound.models import Sound, Author as Voice, Request
 
 
 class IndexPage(TemplateView):
@@ -8,5 +8,7 @@ class IndexPage(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(IndexPage, self).get_context_data(**kwargs)
         context["last_sounds"] = Sound.objects.all()[:10]
+        context["last_voices"] = Voice.objects.all()[:4]
+        context["last_requests"] = Request.objects.all()[:20]
 
         return context
