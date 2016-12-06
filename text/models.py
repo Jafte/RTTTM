26099be18d626ed6a807b79e11c298979d1db787
Jafte <jafte@ya.ru>
2016-12-06 17:32:17 +0300

    author text photo

--- text/models.py
+++ text/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from easy_thumbnails.fields import ThumbnailerImageField
+from uuid_upload_path import upload_to
 from django.urls import reverse
 
 
 class Author(models.Model):
     name = models.CharField(verbose_name=_('name'), max_length=200)
     about = models.TextField(verbose_name=_('about'), blank=True)
+    photo = ThumbnailerImageField(verbose_name=_('photo'), upload_to=upload_to, blank=True)
 
     def __str__(self):
         return '%s' % self.name
