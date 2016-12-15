35fadad62b2b1330d2879e38a2926ea66fcb0c6b
Jafte <jafte@ya.ru>
2016-12-15 03:27:52 +0300

    voice requests fix

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -4,7 +4,7 @@ from user_profile.views import UserDetail, UserList, UserEdit, VoiceRequestCreat
 urlpatterns = [
     url(r'^(?P<username>[\w-]+)/edit/$', UserEdit.as_view(), name="user-profile-edit"),
     url(r'^(?P<username>[\w-]+)/give-me-voice/$', VoiceRequestCreate.as_view(), name="user-voice-request-create"),
-    url(r'^(?P<username>[\w-]+)/voice-куйгуыеы/$', VoiceRequestList.as_view(), name="user-voice-request-list"),
+    url(r'^(?P<username>[\w-]+)/voice-requests/$', VoiceRequestList.as_view(), name="user-voice-request-list"),
     url(r'^(?P<username>[\w-]+)/$', UserDetail.as_view(), name="user-profile-detail"),
     url(r'^$', UserList.as_view(), name="user-profile-list"),
 ]
