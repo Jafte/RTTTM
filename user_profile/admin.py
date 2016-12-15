22b19633f45db8bce2351f1fa20f1b6f671ee660
Jafte <jafte@ya.ru>
2016-12-15 12:46:08 +0300

    voice requests style

--- user_profile/admin.py
+++ user_profile/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from user_profile.models import Profile
+from user_profile.models import Profile, VoiceRequest
 
 admin.site.register(Profile)
+admin.site.register(VoiceRequest)
--- user_profile/forms.py
+++ user_profile/forms.py
@@ -3,10 +3,25 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class ProfileForm(forms.Form):
-    name = forms.CharField(label=_('Name'), max_length=100, required=False)
-    about = forms.CharField(label=_('About'), widget=forms.Textarea, required=False)
-    photo = forms.FileField(label=_('Photo'), required=False)
+    name = forms.CharField(
+        label=_('Name'),
+        widget=forms.TextInput(attrs={'class': 'uk-width-1-1'}),
+        max_length=100,
+        required=False
+    )
+    about = forms.CharField(
+        label=_('About'),
+        widget=forms.Textarea(attrs={'class': 'uk-width-1-1', 'rows': 10}),
+        required=False
+    )
+    photo = forms.FileField(
+        label=_('Photo'),
+        required=False
+    )
 
 
 class WantToReadForm(forms.Form):
-    message = forms.CharField(label=_('Message'), widget=forms.Textarea)
+    message = forms.CharField(
+        label=_('Message'),
+        widget=forms.Textarea(attrs={'class': 'uk-width-1-1', 'rows': 10})
+    )
